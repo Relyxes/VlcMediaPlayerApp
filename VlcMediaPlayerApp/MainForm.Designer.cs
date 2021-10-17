f208b17e6e5751292fc34204cd06d9019d86d687
Артём Пелевин <lynx.other-4x@mail.ru>
2021-10-17 17:45:02 +0500

    Add project files.

---
+++ VlcMediaPlayerApp/MainForm.Designer.cs
@@ -0,0 +1,130 @@
+﻿
+namespace VlcMediaPlayerApp
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.vlcControl = new Vlc.DotNet.Forms.VlcControl();
+            this.buttonPlay = new System.Windows.Forms.Button();
+            this.StopBtn = new System.Windows.Forms.Button();
+            this.OpenBtn = new System.Windows.Forms.Button();
+            this.textBoxUrl = new System.Windows.Forms.TextBox();
+            this.labelUrl = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.vlcControl)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // vlcControl
+            // 
+            this.vlcControl.BackColor = System.Drawing.Color.Black;
+            this.vlcControl.Location = new System.Drawing.Point(12, 12);
+            this.vlcControl.Name = "vlcControl";
+            this.vlcControl.Size = new System.Drawing.Size(776, 385);
+            this.vlcControl.Spu = -1;
+            this.vlcControl.TabIndex = 0;
+            this.vlcControl.Text = "vlcControl";
+            this.vlcControl.VlcLibDirectory = null;
+            this.vlcControl.VlcMediaplayerOptions = null;
+            this.vlcControl.VlcLibDirectoryNeeded += new System.EventHandler<Vlc.DotNet.Forms.VlcLibDirectoryNeededEventArgs>(this.vlcControl_VlcLibDirectoryNeeded);
+            this.vlcControl.Stopped += new System.EventHandler<Vlc.DotNet.Core.VlcMediaPlayerStoppedEventArgs>(this.vlcControl_Stopped);
+            // 
+            // buttonPlay
+            // 
+            this.buttonPlay.Location = new System.Drawing.Point(12, 404);
+            this.buttonPlay.Name = "buttonPlay";
+            this.buttonPlay.Size = new System.Drawing.Size(75, 23);
+            this.buttonPlay.TabIndex = 1;
+            this.buttonPlay.Text = "Play";
+            this.buttonPlay.UseVisualStyleBackColor = true;
+            this.buttonPlay.Click += new System.EventHandler(this.buttonPlay_Click);
+            // 
+            // StopBtn
+            // 
+            this.StopBtn.Location = new System.Drawing.Point(93, 403);
+            this.StopBtn.Name = "StopBtn";
+            this.StopBtn.Size = new System.Drawing.Size(75, 23);
+            this.StopBtn.TabIndex = 1;
+            this.StopBtn.Text = "Stop";
+            this.StopBtn.UseVisualStyleBackColor = true;
+            this.StopBtn.Click += new System.EventHandler(this.StopBtn_Click);
+            // 
+            // OpenBtn
+            // 
+            this.OpenBtn.Location = new System.Drawing.Point(174, 403);
+            this.OpenBtn.Name = "OpenBtn";
+            this.OpenBtn.Size = new System.Drawing.Size(75, 23);
+            this.OpenBtn.TabIndex = 1;
+            this.OpenBtn.Text = "Open";
+            this.OpenBtn.UseVisualStyleBackColor = true;
+            this.OpenBtn.Click += new System.EventHandler(this.OpenBtn_Click);
+            // 
+            // textBoxUrl
+            // 
+            this.textBoxUrl.Location = new System.Drawing.Point(327, 418);
+            this.textBoxUrl.Name = "textBoxUrl";
+            this.textBoxUrl.Size = new System.Drawing.Size(461, 20);
+            this.textBoxUrl.TabIndex = 2;
+            // 
+            // labelUrl
+            // 
+            this.labelUrl.AutoSize = true;
+            this.labelUrl.Location = new System.Drawing.Point(286, 421);
+            this.labelUrl.Name = "labelUrl";
+            this.labelUrl.Size = new System.Drawing.Size(23, 13);
+            this.labelUrl.TabIndex = 3;
+            this.labelUrl.Text = "Url:";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.labelUrl);
+            this.Controls.Add(this.textBoxUrl);
+            this.Controls.Add(this.OpenBtn);
+            this.Controls.Add(this.StopBtn);
+            this.Controls.Add(this.buttonPlay);
+            this.Controls.Add(this.vlcControl);
+            this.Name = "MainForm";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.vlcControl)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Vlc.DotNet.Forms.VlcControl vlcControl;
+        private System.Windows.Forms.Button buttonPlay;
+        private System.Windows.Forms.Button StopBtn;
+        private System.Windows.Forms.Button OpenBtn;
+        private System.Windows.Forms.TextBox textBoxUrl;
+        private System.Windows.Forms.Label labelUrl;
+    }
+}
+
---
+++ VlcMediaPlayerApp/MainForm.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Vlc.DotNet.Core;
+using Vlc.DotNet.Core.Interops.Signatures;
+using Vlc.DotNet.Forms;
+
+namespace VlcMediaPlayerApp
+{
+    public partial class MainForm : Form
+    {
+        FileInfo _filePath;
+
+        public MainForm()
+        {
+            InitializeComponent();
+            textBoxUrl.Text = $@"rtsp://guest:guest@demo.domination.one:7005/camera=2&stream=0";
+            vlcControl.VlcMediaPlayer.Dialogs.UseDialogManager(new MyDialogManager());
+        }
+
+        private void buttonPlay_Click(object sender, EventArgs e)
+        {
+            string uri = textBoxUrl.Text;
+            string[] options =
+            {
+                ":network-caching=2000"
+            };
+            vlcControl.Play(new Uri(uri));
+
+            //if (!vlcControl.VlcMediaPlayer.CouldPlay && uri == string.Empty)
+            //    OpenBtn_Click(this, new EventArgs());
+
+
+            //if (vlcControl.IsPlaying)
+            //{
+            //    vlcControl.Pause();
+            //    buttonPlay.Text = "Play";
+            //}
+            //else
+            //{
+            //    if (uri == string.Empty && _filePath != null)
+            //        vlcControl.Play(_filePath);
+            //    else
+            //        vlcControl.Play(uri);
+
+            //    buttonPlay.Text = "Pause";
+        
+        }
+
+        private void StopBtn_Click(object sender, EventArgs e)
+        {
+            vlcControl.Stop();
+        }
+
+        private void OpenBtn_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog ofd = new OpenFileDialog())
+            {
+                if (ofd.ShowDialog() == DialogResult.OK)
+                    _filePath = new FileInfo(ofd.FileName);
+            }
+            if (_filePath != null)
+                vlcControl.VlcMediaPlayer.Play(_filePath);
+        }
+
+        /// <summary>
+        /// Get library path to vlcmediaplayer.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void vlcControl_VlcLibDirectoryNeeded(object sender, VlcLibDirectoryNeededEventArgs e)
+        {
+            var currentAssembly = Assembly.GetEntryAssembly();
+            var currentDirectory = new FileInfo(currentAssembly.Location).DirectoryName;
+
+            if (currentDirectory == null)
+                return;
+            if (IntPtr.Size == 4)
+                e.VlcLibDirectory = new DirectoryInfo(Path.GetFullPath(@".\libvlc\win-x86\"));
+            else
+                e.VlcLibDirectory = new DirectoryInfo(Path.GetFullPath(@".\libvlc\win-x64\"));
+
+            if (!e.VlcLibDirectory.Exists)
+            {
+                var folderBrowserDialog = new System.Windows.Forms.FolderBrowserDialog();
+                folderBrowserDialog.Description = "Select path to vlclib.dll";
+                folderBrowserDialog.RootFolder = Environment.SpecialFolder.Desktop;
+                folderBrowserDialog.ShowNewFolderButton = true;
+                if (folderBrowserDialog.ShowDialog() == DialogResult.OK)
+                {
+                    e.VlcLibDirectory = new DirectoryInfo(folderBrowserDialog.SelectedPath);
+                }
+                else
+                {
+                    MessageBox.Show("App can't be runned. Don't minimal resources - vlclib.dll.", "Error",
+                        MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+
+        private void vlcControl_Stopped(object sender, Vlc.DotNet.Core.VlcMediaPlayerStoppedEventArgs e)
+        {
+            buttonPlay.BeginInvoke((MethodInvoker)(() => buttonPlay.Text = "Play"));            
+        }
+
+        public class MyDialogManager : IVlcDialogManager
+        {
+            public MyDialogManager()
+            { }
+
+            public async Task DisplayErrorAsync(IntPtr userdata, string title, string text)
+            {
+                MessageBox.Show(text, title, MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+            public async Task<LoginResult> DisplayLoginAsync(IntPtr userdata, IntPtr dialogId, string title, string text, string username, bool askstore, CancellationToken cancellationToken)
+            {
+                var loginResult = new LoginResult
+                {
+                    Username = "guest",
+                    Password = "guest",
+                    StoreCredentials = false
+                };
+                return loginResult;
+            }
+
+            public Task DisplayProgressAsync(IntPtr userdata, IntPtr dialogId, string title, string text, bool indeterminate, float position, string cancelButton, CancellationToken cancellationToken)
+            {
+                return Task.CompletedTask;
+            }
+
+            public Task<QuestionAction?> DisplayQuestionAsync(IntPtr userdata, IntPtr dialogId, string title, string text, DialogQuestionType questionType, string cancelButton, string action1Button, string action2Button, CancellationToken cancellationToken)
+            {
+                return Task.FromResult<QuestionAction?>(null);
+            }
+
+            public void UpdateProgress(IntPtr userdata, IntPtr dialogId, float position, string text)
+            {
+            }
+        }
+    }
+}
